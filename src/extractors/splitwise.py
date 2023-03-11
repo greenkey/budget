4e9a350f97c885575bbb2aaaec6023815f0bb8b9
Lorenzo Mele <lorenzo.mele@octoenergy.com>
2023-03-11 17:36:43 +0100

    fix splitwise tests

--- src/extractors/splitwise.py
+++ src/extractors/splitwise.py
@@ -1,7 +1,7 @@
 import hashlib
 from datetime import date, datetime, timedelta
 from decimal import Decimal
-from typing import Generator, Optional
+from typing import Generator
 
 import splitwise
 from dateutil import parser
@@ -10,30 +10,6 @@ import config
 from src import models
 
 from . import base
-
-# class SplitwiseImporter(base.CsvImporter):
-#     columns = ["Data", "Descrizione", "Categorie", "Costo", "Valuta"]
-#     my_name = "Lorenzo Mele"
-
-#     def get_ledger_items(self) -> Generator[models.LedgerItem, None, None]:
-#         # fields to import: Extra,Amount EUR
-#         for row in self.get_records_from_file():
-#             # parse a date like 04/01/2019
-#             tx_datetime = datetime.fromisoformat(row["Data"])
-#             tx_date = tx_datetime.date()
-#             amount = Decimal(row[self.my_name])
-
-#             yield models.LedgerItem(
-#                 tx_date=tx_date,
-#                 tx_datetime=tx_datetime,
-#                 amount=amount,
-#                 currency=row["Valuta"],
-#                 description=f"{row['Categorie']} - {row['Descrizione']}",
-#                 account="Splitwise",
-#                 ledger_item_type=models.LedgerItemType.INCOME
-#                 if amount > 0
-#                 else models.LedgerItemType.EXPENSE,
-#             )
 
 
 class SplitWiseDownloader(base.Downloader):
--- tests/extractors/test_splitwise.py
+++ tests/extractors/test_splitwise.py
@@ -1,47 +1,11 @@
-from datetime import date, datetime
+import datetime
 from decimal import Decimal
-from pathlib import Path
 from unittest.mock import MagicMock, patch
 
 import pytz
 
 from src import extractors, models
 from tests.extractors.splitwise import factories
-
-splitwise_test_data = """Data,Descrizione,Categorie,Costo,Valuta,Eleonora,Lorenzo Mele
-2023-02-18,Cappuccino ,Cibo e bevande - Altro,3.20,EUR,-1.60,1.60
-2023-02-25,Spesa 13/2 25/2 (10 gg),Alimentari,50.92,EUR,50.92,-50.92
-"""
-
-
-def test_splitwise_importer(tmp_path: Path):
-    splitwise_file = tmp_path / "splitwise.csv"
-    splitwise_file.write_text(splitwise_test_data)
-
-    splitwise_importer = extractors.SplitwiseImporter(splitwise_file)
-    ledger_items = list(splitwise_importer.get_ledger_items())
-    assert sorted(ledger_items) == sorted(
-        [
-            models.LedgerItem(
-                tx_date=date(2023, 2, 18),
-                tx_datetime=datetime(2023, 2, 18, 0),
-                amount=Decimal("1.60"),
-                currency="EUR",
-                description="Cibo e bevande - Altro - Cappuccino ",  # TODO: maybe a "proposed" category?
-                account="Splitwise",
-                ledger_item_type=models.LedgerItemType.INCOME,
-            ),
-            models.LedgerItem(
-                tx_date=date(2023, 2, 25),
-                tx_datetime=datetime(2023, 2, 25, 0),
-                amount=Decimal("-50.92"),
-                currency="EUR",
-                description="Alimentari - Spesa 13/2 25/2 (10 gg)",
-                account="Splitwise",
-                ledger_item_type=models.LedgerItemType.EXPENSE,
-            ),
-        ]
-    )
 
 
 def test_splitwise_online_retriever():
@@ -90,7 +54,7 @@ def test_splitwise_online_retriever():
         [
             models.LedgerItem(
                 tx_id="19bfc6ba6fa1f36c7d69748487cac47607e3befa",
-                tx_date=date(2023, 2, 18),
+                tx_date=datetime.date(2023, 2, 18),
                 tx_datetime=datetime(2023, 2, 18, 10, 41, 51, tzinfo=pytz.utc),
                 amount=Decimal("1.60"),
                 currency="EUR",
@@ -100,7 +64,7 @@ def test_splitwise_online_retriever():
             ),
             models.LedgerItem(
                 tx_id="f044bf2201796a0ff05c3864dfafa91cde931f02",
-                tx_date=date(2023, 2, 25),
+                tx_date=datetime.date(2023, 2, 25),
                 tx_datetime=datetime(2023, 2, 25, 10, 41, 51, tzinfo=pytz.utc),
                 amount=Decimal("-50.92"),
                 currency="EUR",
@@ -116,34 +80,11 @@ def test_splitwise_online_retriever():
     client = MagicMock()
     current_user = factories.CurrentUser()
     client.getCurrentUser.return_value = current_user
-    expenses = [
-        factories.Expense(
-            date="2023-01-19T10:41:51Z",
-            users=[factories.ExpenseUserDict(user__id=current_user.id)],
-        ),
-        factories.Expense(
-            date="2023-01-25T10:41:51Z",
-            users=[factories.ExpenseUserDict(user__id=current_user.id)],
-        ),
-        factories.Expense(
-            date="2023-02-18T10:41:51Z",
-            users=[factories.ExpenseUserDict(user__id=current_user.id)],
-        ),
-        factories.Expense(
-            date="2023-02-25T10:41:51Z",
-            users=[factories.ExpenseUserDict(user__id=current_user.id)],
-        ),
-        factories.Expense(
-            date="2023-03-01T00:00:00Z",
-            users=[factories.ExpenseUserDict(user__id=current_user.id)],
-        ),
-    ]
-    client.getExpenses.return_value = expenses
 
     service = extractors.splitwise.SplitWiseDownloader(client)
 
-    ledger_items = list(service.get_ledger_items(month="2023-02"))
+    list(service.get_ledger_items(month="2023-02"))
 
-    assert set(item.tx_date.month for item in ledger_items) == {
-        2,
-    }
+    client.getExpenses.assert_called_once_with(
+        limit=999, dated_after="2023-02-01", dated_before="2023-03-01"
+    )
